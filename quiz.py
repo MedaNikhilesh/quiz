0a6f104a58f1dd984fa1e18f6896d3e34c004fbf
MedaNikhilesh <107344785+MedaNikhilesh@users.noreply.github.com>
2022-06-16 18:04:49 +0530

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,327 @@
+def main():
+    a = input("").lower().strip()
+    if a == "b":
+        print("correct")
+def national_wide():
+    print("this is national wide quiz")
+    time.sleep(1)
+    print("which city was the capital of andhra pradesh before hyderabad ?")
+    print("A)vijayawada B)kurnool C)warangal")
+    happy=0
+    a = input("").lower().strip()
+    if a == "b":
+        print("correct")
+        happy+=1
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("who is first CM of andhra pradesh")
+    print("A)NT Rama Rao  B)Neelam.Sanjiva reddy  C)Y S Rajasekhara Reddy  ")
+    a = input("").lower().strip()
+    if a == "b":
+        happy+=1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("who is the cm of andhra pradesh in 2018 ?")
+    print("A)YS Jagan B)Chandrababu Naidu C)NT Rama Rao")
+    c = input("").lower().strip()
+    if c == "b":
+        happy+=1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("total number of districts in Andhra Pradesh ?")
+    print("A)10 B)12 C)13")
+    d = input("").lower().strip()
+    if d == "c":
+        happy+=1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("who is the financial minister of india ?")
+    print("A)KB Sahay B)Nirmala C)Y.S.Rajasekhara Reddy")
+    e = input("").lower().strip()
+    if e == "b":
+        happy += 1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("who is the president of india ?")
+    print("A)Narendra Modi B)Ram Nath kovind C)Rahul Gandhi")
+    f = input("").lower().strip()
+    if f == "b":
+        happy += 1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("who is the first PM of India ?")
+    print("A)Jawaharlal Nehru B)Gandhi C)Vallabhbhai Patel")
+    g = input("").lower().strip()
+    if g == "a":
+        happy += 1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("who is the iron man of india ?")
+    print("A)Jawaharlal Nehru B)Gandhi C)Vallabhbhai Patel")
+    i = input("").lower().strip()
+    if i == "c":
+        happy+=1
+        print("correct")
+        print("the national quiz is over you score out of 8 is" + str(happy))
+    else:
+        print("incorrect")
+        print("the national quiz is over you score out of 8 is"+str(happy))
+def sports():
+    print("This sports quiz ")
+    time.sleep(1)
+    print("what is the only sport to be played on the moon ?")
+    print("A)tennis B)football C)golf")
+    score = 0
+    a = input("").lower().strip()
+    if a == "c":
+        score+=1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("who is know as god of cricket ?")
+    print("A)Kapil Dev B)virat kohil C)sachin tendulkar ")
+    b = input("").lower().strip()
+    if b == "c":
+        score+=1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("ronaldo plays for which country ?")
+    print("A)Argentina B)Portugal C)Brazil ")
+    c = input("").lower().strip()
+    if c == "b":
+        score += 1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("Which sports is described as the' beautiful game' ?")
+    print("A)Cricket B)Foot ball C)Golf ")
+    d = input("").lower().strip()
+    if d == "b":
+        score += 1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("which team has highest wining percentage ?")
+    print("A)CSK B)MI C)RCB ")
+    e = input("").lower().strip()
+    if e == "a":
+        score += 1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("which team won most IPL cups ?")
+    print("A)RCB B)MI C)CSK ")
+    f = input("").lower().strip()
+    if f == "b":
+        score += 1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("which team scored most runs in IPL ?")
+    print("A)MI B)CSK C)RCB ")
+    g = input("").lower().strip()
+    if g == "c":
+        score += 1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("sachin was in which IPL team ?")
+    print("A)MI B)CSK C)RCB ")
+    h = input("").lower().strip()
+    if h == "a":
+        score += 1
+        print("correct")
+        print("the sports is over you score out of 8 is" + str(score))
+    else:
+        print("the sports is over you score out of 8 is" + str(score))
+        print("incorrect")
+def GK():
+    print("this GK quiz")
+    time.sleep(1)
+    score=0
+    print("when did world war 1 start ? ")
+    print("A)1914 B)1915 C)1916 ")
+    b=input("").lower().strip()
+    if b == "a":
+        score+=1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("what is tallest statue ?")
+    print("A)statue of unity B)statue of liberty C)spring temple buddha ")
+    a = input("").lower().strip()
+    if a == "a":
+        score+=1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("who is the richest person ?")
+    print("A)Jeff Bezos B)Elon Musk C)Bill Gates ")
+    c = input("").lower().strip()
+    if c == "b":
+        score+=1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("which one is the smallest ocean in the world ?")
+    print("A)Indian ocean b)Pacific ocean c)Arctic ocean ")
+    d = input("").lower().strip()
+    if d == "c":
+        score += 1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("who is know as man of blood and iron ?")
+    print("A)Napoleon B)Bismark C)Sir walter scott ")
+    e = input("").lower().strip()
+    if e == "b":
+        score += 1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("When is Earth Day celebrated ?")
+    print("A)10 April B)7 April C)22 April ")
+    f = input("").lower().strip()
+    if f == "c":
+        score += 1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print(" Name the smallest continent in the world ?")
+    print("A)Australia B)Africa C)Asia ")
+    g = input("").lower().strip()
+    if g == "a":
+        score += 1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("Which continent is called a “Dark continent” ?")
+    print("A)Australia B)Africa C)Asia ")
+    h = input("").lower().strip()
+    if h == "b":
+        score += 1
+        print("correct")
+
+        print("the sports is over you score out of 8 is" + str(score))
+    else:
+        print("incorrect")
+        print("the sports is over you score out of 8 is" + str(score))
+def international():
+    print("This is international wide ")
+    score=0
+    time.sleep(1)
+    print("which is the smallest country ?")
+    print("A)Vatican City B)Bangladesh C)Sri Lanka ")
+    a = input("").lower().strip()
+    if a == "a":
+        score += 1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("which is the most populated country ?")
+    print("A)India B)China C)USA ")
+    b = input("").lower().strip()
+    if b == "b":
+        score += 1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("which country gifted the 'statue of liberty' ?")
+    print("A)canada B)france C)england ")
+    c = input("").lower().strip()
+    if c == "b":
+        score += 1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("which is biggest island in the world ?")
+    print("A)greenland B)finland C)sri lanka ")
+    d = input("").lower().strip()
+    if d == "a":
+        score += 1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("which country has the largest nuclear power ?")
+    print("A)India B)USA C)China ")
+    e = input("").lower().strip()
+    if e == "b":
+        score += 1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("which continent has the highest number of countries ?")
+    print("A)asia B)africa C)europe ")
+    f = input("").lower().strip()
+    if f == "b":
+        score += 1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("which country is known as the 'land of rising sun' ?")
+    print("A)japan B)india C)usa ")
+    g = input("").lower().strip()
+    if g == "a":
+        score += 1
+        print("correct")
+    else:
+        print("incorrect")
+    time.sleep(1)
+    print("only continent in the world without desert is ______")
+    print("A)asia B)europe C)africa ")
+    h = input("").lower().strip()
+    if h == "b":
+        score += 1
+        print("correct")
+        print("the sports is over you score out of 8 is" + str(score))
+    else:
+        print("incorrect")
+        print("the sports is over you score out of 8 is" + str(score))
+import time
+print("This is a computer quiz\nthere are types of quiz\nthere national wide,sports,international wide,GK")
+print("please enter only option")
+print("which quiz do you want to play")
+print("please enter 1 to play national wide,2 for sports,\n3 for GK,4 for international wide")
+z = input("").lower().strip()
+if z == "1":
+    national_wide()
+elif z == "2":
+    sports()
+elif z == "3":
+    GK()
+elif z == "4":
+    international()
